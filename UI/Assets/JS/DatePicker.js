1df2b0815ca3ee37dd003e6fe84886b3b5052133
Joseph Pino <mac110973@gmail.com>
2022-05-11 23:14:26 -0400

    txtDateIssueUpdate

--- UI/Assets/JS/DatePicker.js
+++ UI/Assets/JS/DatePicker.js
@@ -10,6 +10,18 @@
 
 });
 
+$(document).ready(function () {
+
+    $("input[id*=txtDateIssueUpdate]").datepicker({
+
+        dateFormat: "dd/mm/yy",
+        minDate: new Date(2022, 1, 1),
+        maxDate: new Date(2022, 12, 31)
+
+    });
+
+});
+
 //$(document).ready(function() {
 
 //    $('id=txtDateIssueInsert').datepicker({
